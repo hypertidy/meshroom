502a4ede19b669187c23311ec89c24bc54fa3032
Michael Sumner <mdsumner@gmail.com>
2019-09-23 05:23:35 +0000

    document and export main function

--- R/hmm_triangles.R
+++ R/hmm_triangles.R
@@ -1,3 +1,30 @@
+#' Heightmap meshing
+#'
+#' Create an efficient triangulation from a height map.
+#'
+#' Currently input must be a PNG (or JPEG) file name. So an elevation raster
+#' must include values within the range 0,255.
+#' @param x file name, a PNG
+#' @param invert  logical, invert heightmap, default is `FALSE`
+#' @param blur_sigma gaussian blur sigma, default is `0` for no blur
+#' @param border_size border size in pixels, default is `0`
+#' @param border_height border z height, default is `1`
+#' @param base_height solid base height, default is no base `0`
+#' @param max_error maximum triangulation error, default is `0.001`
+#' @param max_triangles maximum number of triangles, default is no maximum `0`
+#' @param max_points maximum number of vertices, default is no maximum `0`
+#' @param z_scale z scale relative to x, y
+#' @param z_exaggeration z exaggeration, default is `1`
+#' @param quiet report metrics from triangulation, default is `TRUE`
+#' @param stl_file if set the triangulation will be written to file, default is no write `""`
+#'
+#' @return nothing at the moment, an empty list
+#' @export
+#'
+#' @examples
+#'library(hmmr)
+#' f <- system.file("extdata/volcano1.png", package = "hmmr", mustWork = TRUE)
+#' hmmr:::hmm_triangles(f)
 hmm_triangles <- function(x,  invert = FALSE,
                           blur_sigma = 0,
                           border_size = 0,
